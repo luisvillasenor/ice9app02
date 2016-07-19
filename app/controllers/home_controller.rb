03f4e010fee1b365466144b9a6ef91ce5cae9c03
Luis Villasenor <luis@iceberg9.com>
2016-07-19 15:20:28 -0500

    actualiza a Orders

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+  	#@products = ShopifyAPI::Product.find(:all, :params => {:limit => 10})
+    #@products = ShopifyAPI::Product.find(:all, :params => {:title => "SA", :limit => 10})
+    @orders = ShopifyAPI::Order.find(:all, :params => {:financial_status => "pending", :limit => 10})
+    #render json: @orders
   end
 end
