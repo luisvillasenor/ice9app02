3481300739a4dfe93f9fcec94aa6a0ffc6e87dff
Luis Villasenor <luis@iceberg9.com>
2016-07-21 21:32:16 -0500

    lista ordenes con algunos campos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < AuthenticatedController
   def index
-  	#@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "name,created_at", :limit => 10})
+  	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "name,created_at", :limit => 10})
     #@products = ShopifyAPI::Product.find(:all, :params => {:title => "SA", :limit => 10})
-    @orders = ShopifyAPI::Order.find(:all, :params => {:financial_status => "pending", :limit => 10})
+    #@orders = ShopifyAPI::Order.find(:all, :params => {:financial_status => "pending", :limit => 10})
     #render json: @orders
   end
 end
