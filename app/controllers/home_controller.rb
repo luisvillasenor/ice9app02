3e364f2e9b7250c8c40587485774e2b93b7c6ef1
Luis Villasenor <luis@iceberg9.com>
2016-07-25 21:27:20 -0500

    actualiza a Orders

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -17,7 +17,7 @@ class HomeController < AuthenticatedController
   end
 
   def show
-  	@order = ShopifyAPI::Order.find(params.require(:name))
+  	@order = ShopifyAPI::Order.find(params(:name))
   	respond_to do |format|
   		format.html # show.html.erb
   		format.json { render json: @order }
