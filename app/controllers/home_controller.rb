5903605cb7f358ba674e881b83a82116256fbb00
Luis Villasenor <luis@iceberg9.com>
2016-08-05 12:46:45 -0500

    buscardor por numero de orden

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -15,7 +15,7 @@ class HomeController < AuthenticatedController
   #end
 
   def show
-  	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "name,created_at,email,financial_status,total_price", :id => params[:name] })
+  	@orders = ShopifyAPI::Order.find(:params => {:fields => "name,created_at,email,financial_status,total_price", :id => params[:name] })
   	render json: @orders
   end
 
