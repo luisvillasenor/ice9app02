b6b45b6d9d7bb4f87bc36f5a1ab2ded45a0aff6d
Luis Villasenor <luis@iceberg9.com>
2016-08-05 12:55:15 -0500

    buscardor por numero de orden

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,13 @@
 class HomeController < AuthenticatedController
   def index
-  	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "name,created_at,email,financial_status,total_price", :limit => 10})
+  	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "id,name,created_at,email,financial_status,total_price", :limit => 10})
     #@products = ShopifyAPI::Product.find(:all, :params => {:title => "SA", :limit => 10})
     #@orders = ShopifyAPI::Order.find(:all, :params => {:financial_status => "pending", :limit => 10})
     #render json: @orders
   end
 
   def pending
- 	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "name,created_at,email,financial_status,total_price", :financial_status => "pending", :limit => 10})
+ 	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "id,name,created_at,email,financial_status,total_price", :financial_status => "pending", :limit => 10})
   end
 
   #def paid
@@ -15,7 +15,7 @@ class HomeController < AuthenticatedController
   #end
 
   def show
-  	@orders = ShopifyAPI::Order.find(:params => {:fields => "name,created_at,email,financial_status,total_price", :name => params[:name] })
+  	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "id,name,created_at,email,financial_status,total_price", :id => params[:id] })
   	render json: @orders
   end
 
