b4d034a60035442349fa6f4ae997ae5f73ba033e
Luis Villasenor <luis@iceberg9.com>
2016-08-05 15:35:51 -0500

    buscardor por numero de orden

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -18,6 +18,10 @@ class HomeController < AuthenticatedController
   def show
   	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "id,name,created_at,email,financial_status,total_price", :name => params[:name], :limit => 250 })
   	#render json: @orders
+  	if @orders.size == 0
+  		flash[:notice] = "Lo siento, no se encontraron registros"
+  		redirect_to(index_path)  		
+  	end
   	render action: 'index'
   end
 
