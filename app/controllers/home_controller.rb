f36d55045c2e326c0ac400c8f6ff5b1984b6899f
Luis Villasenor <luis@iceberg9.com>
2016-08-11 12:34:03 -0500

    redirecciona a Orders Details de la orden

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < AuthenticatedController
   def index
-  	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "id,name,created_at,email,financial_status,total_price", :limit => 250})
+  	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "id,name,created_at,email,financial_status,total_price", :financial_status => "pending", :limit => 250})
     #@products = ShopifyAPI::Product.find(:all, :params => {:title => "SA", :limit => 10})
     #@orders = ShopifyAPI::Order.find(:all, :params => {:financial_status => "pending", :limit => 10})
     #render json: @orders
