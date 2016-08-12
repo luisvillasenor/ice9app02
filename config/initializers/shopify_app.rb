b1d86fbbd882492d74cd87baf7d4f8cc481e634b
Luis Villasenor <luis@iceberg9.com>
2016-08-12 12:21:42 -0500

    Actualiza ConfigShopifyApp

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.api_key = "5106f4a21aa25bbfde5d9abb4ddc902f"
   config.secret = "be9ba61b8923cc912f011e2308ee2438"
-  config.redirect_uri = "https://ice9app02.herokuapp.com/auth/shopify/callback"
+  #config.redirect_uri = "https://ice9app02.herokuapp.com/auth/shopify/callback"
   config.scope = "read_orders, read_products"
   config.embedded_app = true
 end
