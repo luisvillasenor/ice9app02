3aeaf26a399e15cf67d4eda032398bc7af48d8de
Luis Villasenor <luis@iceberg9.com>
2016-08-25 15:17:52 -0500

    Agrega Nombre de la Tienda en Pagina Principal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -21,7 +21,8 @@ class HomeController < AuthenticatedController
   end
 
   def resumen
-    @orders = ShopifyAPI::Order.find(:all, :params => {:fields => "id,name,created_at,email,financial_status,total_price", :name => params[:name], :limit => 1 })
+    #@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "id,name,created_at,email,financial_status,total_price", :name => params[:name], :limit => 1 })
+    @orders = ShopifyAPI::Order.all
   end
 
 end
