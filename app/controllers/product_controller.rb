f1fa928a0124779717c5226428d93a928bdac809
Luis Villasenor <luis@iceberg9.com>
2016-08-31 12:30:54 -0500

    Agrega Nombre de la Tienda en Pagina Principal

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,5 +1,5 @@
 class ProductController < AuthenticatedController
   def index
-    @products = ShopifyAPI::Product.find(:all, :params => {:fields => "id,title,variants[price]", :limit => 250})    
+    @products = ShopifyAPI::Product.find(:all, :params => {:fields => "id,title,variants[:price]", :limit => 250})    
   end
 end
