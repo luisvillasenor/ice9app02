589efce76dceb20af69ef3363ea808771924717c
Luis Villasenor <luis@iceberg9.com>
2016-08-30 21:15:02 -0500

    Agrega Nombre de la Tienda en Pagina Principal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,8 @@ class HomeController < AuthenticatedController
 require 'json'
 
   def index
-    @pufipad = ShopifyAPI::Shop.current
-    #var @pufipad2 = JSON.parse(ShopifyAPI::Shop.current);
+    #var pufipad = ShopifyAPI::Shop.current
+    @pufipad = JSON.stringify(ShopifyAPI::Shop.current);
   end
   def pending
   	@orders = ShopifyAPI::Order.find(:all, :params => {:fields => "id,name,created_at,email,financial_status,total_price", :financial_status => "pending", :limit => 250})
