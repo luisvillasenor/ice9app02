7a33224f411707569af4f25c6dd620ab47a795bf
Luis Villasenor <luis@iceberg9.com>
2016-08-31 11:39:05 -0500

    Agrega Nombre de la Tienda en Pagina Principal

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveResource::Base
-	var mishop = ShopifyAPI::Shop.current
-	var urlshop = mishop.domain
+	mishop = ShopifyAPI::Shop.current
+	urlshop = mishop.domain
 	self.site = "https://" + urlshop + ":3000/"
 end
